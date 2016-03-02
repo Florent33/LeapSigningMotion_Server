7b99bd4d44bc0538e67e1d73c18b0558afe22328
Florent FERRAND <ferrand.florent@gmail.com>
2016-03-02 18:43:26 +0100

    Florent FERRAND
    Commit all my server project (folders and files) from LeapSigningMotion_Server

---
+++ LeapSigningMotion/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace LeapSigningMotion
+{
+    partial class FrameDataForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+/*        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }*/
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayID = new System.Windows.Forms.TextBox();
+            this.displayTimestamp = new System.Windows.Forms.TextBox();
+            this.displayFPS = new System.Windows.Forms.TextBox();
+            this.displayIsValid = new System.Windows.Forms.TextBox();
+            this.displayGestureCount = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.Timestamp = new System.Windows.Forms.Label();
+            this.FPS = new System.Windows.Forms.Label();
+            this.IsValid = new System.Windows.Forms.Label();
+            this.GestureCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayID
+            // 
+            this.displayID.Location = new System.Drawing.Point(334, 93);
+            this.displayID.Name = "displayID";
+            this.displayID.Size = new System.Drawing.Size(206, 20);
+            this.displayID.TabIndex = 0;
+            // 
+            // displayTimestamp
+            // 
+            this.displayTimestamp.Location = new System.Drawing.Point(334, 133);
+            this.displayTimestamp.Name = "displayTimestamp";
+            this.displayTimestamp.Size = new System.Drawing.Size(206, 20);
+            this.displayTimestamp.TabIndex = 1;
+            // 
+            // displayFPS
+            // 
+            this.displayFPS.Location = new System.Drawing.Point(334, 171);
+            this.displayFPS.Name = "displayFPS";
+            this.displayFPS.Size = new System.Drawing.Size(206, 20);
+            this.displayFPS.TabIndex = 2;
+            // 
+            // displayIsValid
+            // 
+            this.displayIsValid.Location = new System.Drawing.Point(334, 213);
+            this.displayIsValid.Name = "displayIsValid";
+            this.displayIsValid.Size = new System.Drawing.Size(206, 20);
+            this.displayIsValid.TabIndex = 3;
+            // 
+            // displayGestureCount
+            // 
+            this.displayGestureCount.Location = new System.Drawing.Point(334, 253);
+            this.displayGestureCount.Name = "displayGestureCount";
+            this.displayGestureCount.Size = new System.Drawing.Size(206, 20);
+            this.displayGestureCount.TabIndex = 4;
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(256, 96);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(18, 13);
+            this.ID.TabIndex = 6;
+            this.ID.Text = "ID";
+            // 
+            // Timestamp
+            // 
+            this.Timestamp.AutoSize = true;
+            this.Timestamp.Location = new System.Drawing.Point(256, 136);
+            this.Timestamp.Name = "Timestamp";
+            this.Timestamp.Size = new System.Drawing.Size(58, 13);
+            this.Timestamp.TabIndex = 7;
+            this.Timestamp.Text = "Timestamp";
+            // 
+            // FPS
+            // 
+            this.FPS.AutoSize = true;
+            this.FPS.Location = new System.Drawing.Point(256, 174);
+            this.FPS.Name = "FPS";
+            this.FPS.Size = new System.Drawing.Size(27, 13);
+            this.FPS.TabIndex = 8;
+            this.FPS.Text = "FPS";
+            // 
+            // IsValid
+            // 
+            this.IsValid.AutoSize = true;
+            this.IsValid.Location = new System.Drawing.Point(256, 216);
+            this.IsValid.Name = "IsValid";
+            this.IsValid.Size = new System.Drawing.Size(38, 13);
+            this.IsValid.TabIndex = 9;
+            this.IsValid.Text = "IsValid";
+            // 
+            // GestureCount
+            // 
+            this.GestureCount.AutoSize = true;
+            this.GestureCount.Location = new System.Drawing.Point(256, 256);
+            this.GestureCount.Name = "GestureCount";
+            this.GestureCount.Size = new System.Drawing.Size(72, 13);
+            this.GestureCount.TabIndex = 10;
+            this.GestureCount.Text = "GestureCount";
+            // 
+            // FrameDataForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 435);
+            this.Controls.Add(this.GestureCount);
+            this.Controls.Add(this.IsValid);
+            this.Controls.Add(this.FPS);
+            this.Controls.Add(this.Timestamp);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.displayGestureCount);
+            this.Controls.Add(this.displayIsValid);
+            this.Controls.Add(this.displayFPS);
+            this.Controls.Add(this.displayTimestamp);
+            this.Controls.Add(this.displayID);
+            this.Name = "FrameDataForm";
+            this.Text = "WinFormSample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox displayID;
+        private System.Windows.Forms.TextBox displayTimestamp;
+        private System.Windows.Forms.TextBox displayFPS;
+        private System.Windows.Forms.TextBox displayIsValid;
+        private System.Windows.Forms.TextBox displayGestureCount;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label Timestamp;
+        private System.Windows.Forms.Label FPS;
+        private System.Windows.Forms.Label IsValid;
+        private System.Windows.Forms.Label GestureCount;
+
+    }
+}
+
---
+++ LeapSigningMotion/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using Leap;
+
+namespace LeapSigningMotion
+{
+    public partial class FrameDataForm : Form, ILeapEventDelegate
+    {
+
+        private Controller controller;
+        private LeapEventlistener listener;
+        public FrameDataForm()
+        {
+            InitializeComponent();
+            this.controller = new Controller();
+            this.listener = new LeapEventlistener(this);
+            controller.AddListener(listener);
+        }
+
+        delegate void LeapEventDelegate(string eventName);
+        private void textBox1_TextChanged(object sender, EventArgs e) {}
+        private void Form1_Load(object sender, EventArgs e) {}
+
+        public void LeapEventNotification(string EventName)
+        {
+            if (!this.InvokeRequired)
+            {
+                switch (EventName)
+                {
+                    case "onInit":
+                        Debug.WriteLine("Init");
+                        break;
+
+                    case "onConnect":
+                        this.connectHandler();
+                        break;
+
+                    case "onFrame":
+                        if (!this.Disposing)
+                            this.newFrameHandler(this.controller.Frame());
+                        break;
+                }
+            }
+            else
+            {
+                BeginInvoke(new LeapEventDelegate (LeapEventNotification), new object[] {EventName});
+            }
+        }
+
+        void connectHandler()
+        {
+            this.controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+            this.controller.Config.SetFloat("Gesture.Circle.MinRadius", 40.0f);
+            this.controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+        }
+
+        void newFrameHandler(Frame frame)
+        {
+            this.displayID.Text = frame.Id.ToString();
+            this.displayTimestamp.Text = frame.Timestamp.ToString();
+            this.displayFPS.Text = frame.CurrentFramesPerSecond.ToString();
+            this.displayIsValid.Text = frame.IsValid.ToString();
+            this.displayGestureCount.Text = frame.Gestures().Count.ToString();
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing)
+                {
+                    if (components != null)
+                    {
+                        components.Dispose();
+                    }
+                    this.controller.RemoveListener(this.listener);
+                    this.controller.Dispose();
+                }
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
+    }
+
+    public interface ILeapEventDelegate
+    {
+        void LeapEventNotification(string EventName);
+    }
+
+    public class LeapEventlistener : Listener
+    {
+        ILeapEventDelegate eventDelegate;
+
+        public LeapEventlistener(ILeapEventDelegate delegateObject)
+        {
+            this.eventDelegate = delegateObject;
+        }
+
+        public override void OnInit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onInit");
+        }
+
+        public override void OnConnect(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onConnect");
+        }
+
+        public override void OnFrame(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onFrame");
+        }
+
+        public override void OnExit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onExit");
+        }
+
+        public override void OnDisconnect(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onDisconnect");
+        }
+    }
+}
